48c9ce232bb1953ce671f4c9bd27f33132cdece4
Vladislav Prozorov <93047063+vprozorov93@users.noreply.github.com>
2022-09-02 15:05:09 +0300

    fixed bugs

--- slider/task.js
+++ slider/task.js
@@ -1,51 +1,41 @@
 const sliderPrevButton = document.querySelector('.slider__arrow_prev')
 const sliderNextButton = document.querySelector('.slider__arrow_next')
-
 const imgArray = Array.from(document.querySelectorAll('.slider__item'))
 const sliderDots = Array.from(document.querySelectorAll('.slider__dot'))
-let activeImg = document.querySelector('.slider__item_active')
-let activeDot = document.querySelector('.slider__dot_active')
 
-sliderNextButton.onclick = ()  => {
-    const activeImgIndex = imgArray.indexOf(activeImg)
-    imgArray[activeImgIndex].className = 'slider__item'
-    sliderDots[activeImgIndex].className = 'slider__dot'
-    if (imgArray.length-1 == activeImgIndex) {
-        imgArray[0].className = 'slider__item slider__item_active'
-        sliderDots[0].className = 'slider__dot slider__dot_active'
-        activeImg = imgArray[0]
-    } else {
-        imgArray[activeImgIndex+1].className = 'slider__item slider__item_active'
-        sliderDots[activeImgIndex+1].className = 'slider__dot slider__dot_active'
-        activeImg = imgArray[activeImgIndex+1]
+function changeImg(event) {
+    const activeElementIndex = sliderDots.indexOf(document.querySelector('.slider__dot_active'))
+    function executeChange(clickedElementIndex, activeElementIndex){
+        imgArray[clickedElementIndex].classList.add('slider__item_active')
+        imgArray[activeElementIndex].classList.remove('slider__item_active')
+        sliderDots[clickedElementIndex].classList.add('slider__dot_active')
+        sliderDots[activeElementIndex].classList.remove('slider__dot_active')
     }
-}
+    
+    if (Array.from(event.currentTarget.classList).includes("slider__dot")) {
+        let clickedElementIndex = sliderDots.indexOf(event.currentTarget)
+        executeChange(clickedElementIndex, activeElementIndex)
+    } 
 
-sliderPrevButton.onclick = ()  => {
-    const activeImgIndex = imgArray.indexOf(activeImg)
-    imgArray[activeImgIndex].className = 'slider__item'
-    sliderDots[activeImgIndex].className = 'slider__dot'
-    if (activeImgIndex == 0) {
-        imgArray[imgArray.length-1].className = 'slider__item slider__item_active'
-        sliderDots[imgArray.length-1].className = 'slider__dot slider__dot_active'
-        activeImg = imgArray[imgArray.length-1]
-    } else {
-        imgArray[activeImgIndex-1].className = 'slider__item slider__item_active'
-        sliderDots[activeImgIndex-1].className = 'slider__dot slider__dot_active'
-        activeImg = imgArray[activeImgIndex-1]
+    if (Array.from(event.currentTarget.classList).includes("slider__arrow_next")) {
+        let clickedElementIndex = activeElementIndex+1
+        if (clickedElementIndex > sliderDots.length-1) {
+            clickedElementIndex = 0
+        }
+        executeChange(clickedElementIndex, activeElementIndex)
     }
-}
 
+    if (Array.from(event.currentTarget.classList).includes("slider__arrow_prev")) {
+        let clickedElementIndex = activeElementIndex-1
+        if (clickedElementIndex < 0) {
+            clickedElementIndex = sliderDots.length-1
+        }
+        executeChange(clickedElementIndex, activeElementIndex)
+    }
+}   
+
+sliderNextButton.addEventListener('click', changeImg)
+sliderPrevButton.addEventListener('click', changeImg)
 sliderDots.forEach((element)=>{
-    element.onclick = () => {
-        const activeDotIndex = sliderDots.indexOf(activeDot)
-        const clickedDotIndex = sliderDots.indexOf(element)
-
-        sliderDots[activeDotIndex].className = 'slider__dot'
-        sliderDots[clickedDotIndex].className = 'slider__dot slider__dot_active'
-        activeDot = element
-
-        imgArray[activeDotIndex].className = 'slider__item'
-        imgArray[clickedDotIndex].className = 'slider__item slider__item_active'
-    }
+    element.addEventListener('click', changeImg)
 })
